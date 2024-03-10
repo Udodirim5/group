209143627b1b089b4d59bfd3a2c5f168b1427354
Udodirim <Udodirimwisdom@gmail.com>
2024-03-10 11:13:53 +0100

    coming together

--- public/js/add-new-user.js
+++ public/js/add-new-user.js
@@ -1,15 +1,3 @@
-let fullName;
-let username;
-let email;
-let gender;
-let dateOfBirth;
-let password;
-let country;
-let stateProvince;
-let city;
-let phoneNumber;
-let whatsapp;
-
 const selectedImgPrev = document.querySelector('#selected-img-prev');
 const selectedImgInput = document.querySelector('#file-upload');
 const imgPreview = document.querySelector('.img-preview');
@@ -23,102 +11,6 @@ selectedImgInput.addEventListener('change', () => {
 submitBtn.addEventListener("click", async (e) => {
     e.preventDefault();
 
-    // Retrieve input values
-    fullName = document.getElementById("fullname").value;
-    username = document.getElementById("username").value;
-    email = document.getElementById("email").value;
-    dateOfBirth = document.getElementById("date_of_birth").value;
-    password = document.getElementById("password").value;
-    country = document.getElementById("country").value;
-    stateProvince = document.getElementById("state").value;
-    city = document.getElementById("city").value;
-    phoneNumber = document.getElementById("phone").value;
-    whatsapp = document.getElementById("whatsapp").value;
-
-    // Reset error messages
-    document.getElementById("error_fullname").textContent = "";
-    document.getElementById("error_username").textContent = "";
-    document.getElementById("error_email").textContent = "";
-    document.getElementById("error_date_of_birth").textContent = "";
-    document.getElementById("error_password").textContent = "";
-    document.getElementById("error_country").textContent = "";
-    document.getElementById("error_state").textContent = "";
-    document.getElementById("error_city").textContent = "";
-    document.getElementById("error_phone").textContent = "";
-    document.getElementById("error_whatsapp").textContent = "";
-
-    // Validate each input field
-    const errors = [];
-    if (fullName.trim() === "") {
-        errors.push("Please enter your Full Name.");
-    }
-    if (username.trim() === "") {
-        errors.push("Please enter your username.");
-    }
-    if (email.trim() === "") {
-        errors.push("Please enter your Email.");
-    }
-    if (gender == "") {
-        errors.push("Please select your gender.");
-    }
-    if (dateOfBirth.trim() === "") {
-        errors.push("Please enter your date of birth.");
-    }
-    if (password.trim() === "") {
-        errors.push("Please enter your password.");
-    }
-    if (country.trim() === "") {
-        errors.push("Please enter your country.");
-    }
-    if (stateProvince.trim() === "") {
-        errors.push("Please enter your state.");
-    }
-    if (city.trim() === "") {
-        errors.push("Please enter your city.");
-    }
-    if (phoneNumber.trim() === "") {
-        errors.push("Please enter your phone number.");
-    }
-    if (whatsapp.trim() === "") {
-        errors.push("Please enter your whatsapp number.");
-    }
-
-    // Display errors if any
-    if (errors.length > 0) {
-        errors.forEach(error => {
-            // Create a new error message element and append it to the corresponding error container
-            const errorMessage = document.createElement("div");
-            errorMessage.textContent = error;
-            errorMessage.classList.add("error-message");
-            document.getElementById(`error_${error.split(" ")[2].toLowerCase()}`).appendChild(errorMessage);
-        });
-    } else {
-        // All inputs are valid, proceed with your logic here
-        const inputData = {
-            fullName,
-            username,
-            email,
-            gender,
-            dateOfBirth,
-            password,
-            country,
-            stateProvince,
-            city,
-            phoneNumber,
-            whatsapp
-        };
-        console.log(inputData);
-
-    }
-    
-    // Disable the button if any required field is empty
-    if (fullName === "" || username === "" || email === "" || dateOfBirth === "" || password === "" || country === "" || stateProvince === "" || city === "" || phoneNumber === "" || whatsapp === "") {
-        submitBtn.disabled = true;
-    }
-
-
-
-
 
         if (isFormLocked) {
             return null;
@@ -127,15 +19,29 @@ submitBtn.addEventListener("click", async (e) => {
         isFormLocked = true
 
 
+        const inputData = {
+            fullName: document.getElementById("fullname").value,
+            username: document.getElementById("username").value,
+            email: document.getElementById("email").value,
+            gender: document.getElementById("gender").value,
+            dateOfBirth: document.getElementById("date_of_birth").value,
+            password: document.getElementById("password").value,
+            country: document.getElementById("country").value,
+            stateProvince: document.getElementById("state").value,
+            city: document.getElementById("city").value,
+            phoneNumber: document.getElementById("phone").value,
+            whatsapp: document.getElementById("whatsapp").value
+        };
+
         // if (cloudinaryReturnedObject) {
-        //     pet.public_id = cloudinaryReturnedObject.public_id;
-        //     pet.version = cloudinaryReturnedObject.version;
-        //     pet.signature = cloudinaryReturnedObject.signature;
+        //     inputData.public_id = cloudinaryReturnedObject.public_id;
+        //     inputData.version = cloudinaryReturnedObject.version;
+        //     inputData.signature = cloudinaryReturnedObject.signature;
         // }
 
         // document.querySelector(`#add-new-pet-form`).classList.add("form-loading");
 
-        const ourPromise = await fetch("/.netlify/functions/addPet", {
+        const ourPromise = await fetch("/.netlify/functions/addNewUser", {
             method: "POST",
             body: JSON.stringify(inputData),
             headers: { "Content-Type": "application/json" },
@@ -144,6 +50,6 @@ submitBtn.addEventListener("click", async (e) => {
         const theRespnose = await ourPromise.json();
 
         if (theRespnose.success) {
-            window.location = "/admin/admin-users";
+            window.location = "/admin/admin-user";
         }
     });
---
+++ public/js/dynamicData.js
@@ -0,0 +1,23 @@
+const template = document.querySelector('#new-user-card');
+const wrapper = document.createDocumentFragment()
+async function addNewUsers() {
+    // const usersPromise = await fetch("/.netlify/functions/users");
+    const usersPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json");
+    const userData = await usersPromise.json();
+
+    userData.forEach((user) => {
+        const clone = template.content.cloneNode(true);
+        clone.querySelector('#users-name').textContent = "Name: " + user.name;
+        clone.querySelector('#users-email').textContent = "Email: " + user.email;
+        clone.querySelector('#users-phone').textContent = "Phone: " + user.phone;
+        clone.querySelector('#users-whatsapp').textContent = "Whatsapp: " + user.whatsapp;
+        clone.querySelector('.img-side img').src = user.photo;
+        clone.querySelector('.img-side img').alt = `profile photo of ${user.name}`; 
+        wrapper.appendChild(clone);
+    })
+
+    document.querySelector(".added-user-list").appendChild(wrapper);
+
+
+}
+addNewUsers();
